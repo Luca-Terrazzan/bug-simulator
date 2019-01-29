42af6d8fe5b08f2e442d02217ef7bff1c1f32155
Luca Terrazzan <luca.terrazzan@docebo.com>
2019-01-29 21:22:47 +0100

    main bug lifecycle test

--- src/bug/test_bug.py
+++ src/bug/test_bug.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import Mock, patch
 
+from datetime import date
 from .bug import Bug
 from .enums import *
 
@@ -23,11 +24,38 @@ class TestBug(unittest.TestCase):
             self.assertEquals(dateMngrMock.today.call_count, 0, 'Dependency called')
 
     def test_bug_lifecycle(self):
-        dateMngrMock = Mock()
+        with patch('src.datemngr.date_mngr.DateMngr') as DateMngrMock:
+            dateMngrMock = DateMngrMock()
+            dateMngrMock.today.return_value =date(year=2019, month=1, day=1)
+
         bug = Bug(Priority.LOW, 0.5, Technology.LEGACY, Type.BACKEND, dateMngrMock)
         self.assertTrue(dateMngrMock.today.assert_called_once, 'Creation date not set')
 
+        # Assign the bug an ETA
+        eta = date(year=2019, month=1, day=16)
+        bug.assignEta(eta)
+        self.assertEquals(bug.status, Status.ETA)
 
+        # Put the bug in progress by a dev
+        with patch('src.team.dev.dev.Dev') as DevMock:
+            dev = DevMock()
+        bug.start(dev)
+        self.assertEquals(bug.status, Status.INPROGRESS)
+
+        # Dev completes fix
+        bug.finish()
+        self.assertEquals(bug.status, Status.CODEREVIEW)
+
+        # Another dev performs the code review
+        with patch('src.team.dev.dev.Dev') as DevMock:
+            reviewer = DevMock()
+            reviewer.type = Type.BACKEND
+        bug.review(reviewer)
+        self.assertEquals(bug.status, Status.QA)
+
+        # QC performs testing
+        bug.release()
+        self.assertEquals(bug.status, Status.DONE)
 
 if __name__ == '__main__':
     unittest.main()
